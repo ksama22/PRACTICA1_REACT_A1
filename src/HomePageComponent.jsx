247ee015ca3f0bca00e33da129a6c7164fe04b6d
Kevin Sama <113372511+ksama22@users.noreply.github.com>
2023-01-29 21:31:59 +0100

    Activitat 1 (#1)
    * Buida els elements per defecte
    
    * empty default project
    
    * Components React
    
    * Activitat 1

---
+++ src/HomePageComponent.jsx
@@ -0,0 +1,24 @@
+//javascript minuscula
+export const InfoComponent = () => {
+  return <h1>Grup Kevin Sama y Raul Rufo</h1>;
+};
+export const InputComponent = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Digam alguna dada" />
+    </div>
+  );
+};
+
+//component mayuscula
+/*
+const UserComponent = (props) => {
+  return (
+    <div>
+      <h1>
+        Buenas {props.name} {props.surname} de {props.age} años
+      </h1>
+    </div>
+  );
+};
+*/
